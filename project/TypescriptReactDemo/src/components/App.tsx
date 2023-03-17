a9a55568944f3d7e6545832bb8f550c6a9dcef18
Alejandro F <afuste7@gmail.com>
2023-03-17 17:28:47 -0400

    removed hello tsx files and added new code to App.tsx file

--- project/TypescriptReactDemo/src/components/App.tsx
+++ project/TypescriptReactDemo/src/components/App.tsx
@@ -1,13 +1,35 @@
 import * as React from "react";
 
-import Hello from "./Hello";
-import HelloWithHooks from "./HelloWithHooks";
+interface IInvoiceListProps {
+  invoiceData: {
+    customerName: string;
+    invoices: {id: number; name: string, total: string}[]
+  }
+  
+  logo?: string;
+}
+
+const InvoiceList = (props: IInvoiceListProps) => {
+  const {customerName, invoices} = props.invoiceData;
+  
+  return <div>
+    <h1>{props.invoiceData.customerName}</h1>
+  </div>
+}
 
 const App = () => {
+  const data = {
+    customerName: "Google",
+    invoices: [
+      {id: 123, name: "Dev work", total: "20.00"},
+      {id: 456, name: "More dev work", total: "50.00"},
+      {id: 456, name: "Something different", total: "100.00"},
+    ]
+  }
+
   return (
     <div>
-      <Hello compiler={"TS"} framework={"React"} />
-      <HelloWithHooks />
+      <InvoiceList invoiceData={data}/>
     </div>
   );
 };
